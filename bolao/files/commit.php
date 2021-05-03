28a915d894cf3cdc63e8db8ab3cc6cac000e405f
talesCPV <tales@flexibus.com.br>
2021-05-03 11:45:08 -0300

    by_script -> seg mai  3 11:45:07 -03 2021

--- bolao/files/commit.php
+++ bolao/files/commit.php
@@ -23,7 +23,43 @@
 
         }else if($_POST["do"] == 3){
             $auth = $_POST["auth"];
-            $query = "SELECT * FROM tb_usuario WHERE master_key='{$auth}';";       
+            $query = "SELECT * FROM tb_usuario WHERE master_key='{$auth}';";
+
+        }else if($_POST["do"] == 4){
+            $json = $_POST["json"];
+            $ano = $_POST["ano"];
+            $obj = json_decode($json);
+            for($i=0; $i<count($obj); $i++){
+                for($j=0; $j<count($obj[$i]); $j++){
+                    $jogo = get_object_vars($obj[$i][$j]);
+                    $mand = get_object_vars($jogo["mandante"]);
+                    $vist = get_object_vars($jogo["visitante"]);
+                    //*****//
+                    $num_jogo = $jogo["num"];
+                    $data = $jogo["data"];
+                    $horario = $jogo["horario"];
+                    $local = $jogo["estadio"];
+                    $mand_nome = $mand["nome"];
+                    $mand_sigla = $mand["sigla"];
+                    $mand_logo = $mand["logo"];
+                    $mand_placar = $mand["placar"];
+                    $vist_nome = $vist["nome"];
+                    $vist_sigla = $vist["sigla"];
+                    $vist_logo = $vist["logo"];
+                    $vist_placar = $vist["placar"];
+
+
+                    $query = "INSERT INTO tb_jogos VALUES (DEFAULT, {$ano}, {$num_jogo}, '{$horario}', '{$data}', '{$local}', DEFAULT, '{$mand_nome}', '{$vist_nome}', '{$mand_placar}', '{$vist_placar}');";
+                    mysqli_query($conexao, $query);
+
+//                    print("Jogo: {$num_jogo} Data: {$data} Hora: {$horario} Local: {$local} - {$mand_nome} {$mand_placar} X {$vist_placar} {$vist_nome}<br>");
+//                    print("Mandante: ".$mand["nome"]."<br>");
+                            
+                }
+            }
+//            print $json;
+
+//            $query = "SELECT * FROM tb_usuario WHERE master_key='{$auth}';";                 
         }
 
 //        echo $query;
--- bolao/js/main.js
+++ bolao/js/main.js
@@ -1,33 +1,164 @@
 
-//Object Cookie
-checklog(sessionStorage.getItem("auth"));
+  // Variáveis
 
-//Object Menu
-const menu = new Object;
-menu.dashboard = document.getElementById('menuDash');
-menu.rodada = document.getElementById('menuRod');
-menu.logout = document.getElementById('menuOut');
+  const ano = 2020
+  const auth = sessionStorage.getItem("auth");
 
-menu.dashboard.addEventListener('click',()=>{
-//    console.log( cookies.auth);
-})
+  let campeonato = [];
+  getScore(ano);
+  console.log(campeonato);
+  
+  //Object Cookie
+  checklog(sessionStorage.getItem("auth"));
+  
+  //Object Menu
+  const menu = new Object;
+  menu.dashboard = document.getElementById('menuDash');
+  menu.rodada = document.getElementById('menuRod');
+  menu.logout = document.getElementById('menuOut');
+  
+  menu.dashboard.addEventListener('click',()=>{
+
+    alert(JSON.stringify( campeonato ));
 
 
-menu.rodada.addEventListener('click',()=>{
-    console.log(sessionStorage.getItem("auth") );
-})
+    const data = new URLSearchParams();
+    data.append("do", "4");
+    data.append("json", JSON.stringify( campeonato ));
+    data.append("ano", 2020);
+
+    const myRequest = new Request("files/commit.php",{
+        method : "POST",
+        body : data
+    })
+
+    const resp = fetch(myRequest);
 
 
-menu.logout.addEventListener('click',()=>{
-    sessionStorage.setItem("auth", "");
-    window.location.replace("index.html");
-   
-})
-
-function checklog(auth){
-
-    if(auth == ""){
-        window.location.replace("index.html");
+  })
+  
+  
+  menu.rodada.addEventListener('click',()=>{
+      console.log(auth );
+  })
+  
+  
+  menu.logout.addEventListener('click',()=>{
+      sessionStorage.setItem("auth", "");
+      window.location.replace("index.html");
+     
+  })
+  
+  function checklog(auth){
+      if(auth == null || auth == ""){
+          window.location.replace("index.html");
+      }
+  }
+  
+  function openMenu() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
     }
-
-}
+    
+  const btnVer = document.getElementById('btnVer');
+  
+/*  
+  btnVer.addEventListener('click',()=>{
+      const rod = campeonato[edtRod.value - 1];
+  
+      let tab = "<table>";
+  
+      for(let i=0; i< rod.length; i++){
+          console.log(rod[i]);
+          tab += `
+              <tr>
+                  <td>${rod[i].data}</td>            
+                  <td><img src="${rod[i].mandante.logo}" width="50" height="50"></td>
+                  <td>${rod[i].mandante.placar}</td>
+                  <td>x</td>
+                  <td>${rod[i].visitante.placar}</td>
+                  <td><img src="${rod[i].visitante.logo} width="50" height="50""></td>
+              </tr>
+          
+          `;
+      }
+      tab += "</table>";
+  
+      document.getElementById("divTabela").innerHTML = tab;
+  
+  })
+*/  
+  function getScore(ano){
+  
+      campeonato = [];
+      const url = 'https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/'+ano;
+      
+      fetch(url)
+      .then(function (response) {
+          response.text().then((text)=>{
+              const cbf = document.createElement('div');
+              cbf.innerHTML =  text;
+  
+              for(let i=0; i<38; i++){
+                  const rod = cbf.querySelector(`[data-slide-index="${i}"]`);
+                  const jogos = rod.getElementsByClassName("clearfix");
+                  const local = rod.getElementsByClassName("partida-desc text-1 color-lightgray block uppercase text-center");
+      
+                  campeonato.push([]);
+  
+                  for(let j=0; j<10; j++){
+  
+                      const jogo = new Object;
+  
+                      const vist = jogos[j].getElementsByClassName("time pull-right");
+                      const mand = jogos[j].getElementsByClassName("time pull-left");
+                      const placar = jogos[j].querySelector('.label-2');
+  
+  //                    console.log(local[j * 2].firstChild.nodeValue)
+  
+                      jogo.num = local[j * 2].firstChild.nodeValue.split(' - ')[1].split(': ')[1].trim();
+                      jogo.dia_sem = local[j * 2].firstChild.nodeValue.split(' - ')[0].trim().split(' ')[0];
+                      jogo.data = local[j * 2].firstChild.nodeValue.split(' - ')[0].trim().split(' ')[1];
+                      jogo.horario = local[j * 2].firstChild.nodeValue.split(' - ')[0].trim().split(' ')[2];
+                      jogo.estadio = local[j * 2 + 1].firstChild.nodeValue.trim().split(' - ')[0];
+                      jogo.cidade = local[j * 2 + 1].firstChild.nodeValue.trim().split(' - ')[1];
+                      jogo.uf = local[j * 2 + 1].firstChild.nodeValue.trim().split(' - ')[2];
+  
+                      jogo.mandante = new Object;
+                      jogo.mandante.sigla = mand[0].getElementsByClassName("time-sigla")[0].innerHTML;
+                      jogo.mandante.logo = mand[0].querySelector(["img"]).src;
+                      jogo.mandante.nome = mand[0].querySelector(["img"]).title.split(' - ')[0];
+                      jogo.mandante.uf = mand[0].querySelector(["img"]).title.split(' - ')[1];
+                      
+                      if(placar != null){
+                          jogo.mandante.placar = placar.innerHTML.split(' x ')[0];        
+                      }else{
+                          jogo.mandante.placar = ' ';
+                      }
+            
+                      jogo.visitante = new Object;
+                      jogo.visitante.sigla = vist[0].getElementsByClassName("time-sigla")[0].innerHTML;
+                      jogo.visitante.logo = vist[0].querySelector(["img"]).src;
+                      jogo.visitante.nome = vist[0].querySelector(["img"]).title.split(' - ')[0];
+                      jogo.visitante.uf = vist[0].querySelector(["img"]).title.split(' - ')[1];
+  
+                      if(placar != null){
+                          jogo.visitante.placar = placar.innerHTML.split(' x ')[1];        
+                      }else{
+                          jogo.visitante.placar = ' ';
+                      }
+  
+                      campeonato[i].push(jogo);
+  
+                  }
+  
+              }
+          
+          })
+      });
+  
+  }  
